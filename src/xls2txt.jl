b0327231b018f5ef94fa2f0f2b452ef3613f1d78
Peter Bräuer <pb866.git@gmail.com>
2019-12-09 13:28:43 +0100

    Correct low altitude values
    Only correct altitude values of 1000ft and above, where a
    comma-separator is found and multiply by 1000. Leave low altitude values
    unchanged.
    New function findFiles to find Excel files recursively in given folder
    and save file names and folder paths.
    Restructure function correctXLS and move code to separate subroutines.
    Add Juno.input and use instead of readlines().

--- src/xls2txt.jl
+++ src/xls2txt.jl
@@ -6,11 +6,76 @@ import ExcelFiles; const xls = ExcelFiles
 import CSV
 import DataFrames.DataFrame
 import ProgressMeter; const pm = ProgressMeter
+import Juno.input
 
 export logg, correctXLS
 
 
 readXLS(filename, sheet::String="Tabelle1") = DataFrame(xls.load(filename, sheet))
+
+
+"""
+    findFiles(inventory::Vector{String}, folder::String, filetypes::String...) -> inventory
+
+Load inventory files saved in `folder` to the `inventory` holding the file names
+and locations ending with `filetypes` as a vector of strings.
+"""
+function findFiles(inventory::Vector{String}, folders::Vector{String},
+  folder::String, basefolder::String, filetypes::String...)
+  # Construct Regex of file endings from filetypes
+  fileendings = Regex(join(filetypes,'|'))
+  # Scan directory for files and folders and save directory
+  dir = readdir(folder); path = normpath(folder)
+  for cwd in dir
+    # Save current directory/file
+    file = joinpath(path, cwd)
+    if endswith(file, fileendings)
+      # Save files of correct type
+      push!(inventory, basename(file))
+      push!(folders, relpath(dirname(file), basefolder))
+    elseif isdir(file)
+      # Step into subdirectories and scan them, too
+      inventory, folders = findFiles(inventory, folders, file, basefolder, filetypes...)
+    end
+  end
+
+  return inventory, folders
+end # function findcsv
+
+function prepare_outfolder(outfolder::String)
+  if isdir(outfolder)
+    println("\33[95mOutput folder already exists. Overwrite content?")
+    confirm = input("(\33[4mY\33[0m\33[95mes/\33[4mN\33[0m\33[95mo)\33[0m ")
+    if startswith(lowercase(confirm), "y")
+      rm(outfolder, recursive=true); mkpath(outfolder)
+    else
+      @error "Cancel overwrite"
+    end
+  else
+    println("$outfolder created."); mkpath(outfolder)
+  end
+end #function prepare_outfolder
+
+
+function processXLS(files::Vector{String}, folders::Vector{String},
+  infolder::String, outfolder::String, sheetname::String)
+  mkpath.(joinpath.(outfolder, unique(folders)))
+  pm.@showprogress 0.1 "convert xls to text..." for (file, folder) in zip(files, folders)
+    try xlsdata = readXLS(joinpath(infolder, folder, file), sheetname)
+      valid = @. (!ismissing(xlsdata.Latitude)) & !(xlsdata.Latitude isa String) &
+        (!ismissing(xlsdata.Longitude)) & !(xlsdata.Longitude isa String) &
+        (!ismissing(xlsdata.feet)) & !(xlsdata.feet isa String)
+      xlsdata = xlsdata[valid,:]
+      xlsdata.Latitude ./= 1e4
+      xlsdata.Longitude ./= 1e4
+      xlsdata.feet = [x < 50 ? x * 1000 : x for x in xlsdata.feet]
+      CSV.write(splitext(joinpath(outfolder, folder, file))[1]*".dat", xlsdata, delim='\t')
+    catch
+      @warn "Error in reading data from $folder/$file. Possibly empty Excel sheets. Data skipped."
+    end
+  end
+end #function processXLS
+
 
 """
     correctXLS(infolder, outfolder, sheetname::String="Tabelle1")
@@ -21,24 +86,10 @@ that is read in.
 """
 function correctXLS(infolder::String, outfolder::String,
   sheetname::String="Tabelle1")
-  if !isdir(outfolder)
-    println("$outfolder created."); mkpath(outfolder)
-  end
-  folders = readdir(infolder)[isdir.(infolder.*"/".*readdir(infolder))]
-  pm.@showprogress 1 "convert xls to text..." for folder in folders
-    files = readdir(joinpath(infolder, folder))
-    mkdir(joinpath(outfolder, folder))
-    for file in files
-      try xlsdata = readXLS(joinpath(infolder, folder, file))
-        xlsdata.Latitude ./= 1e4
-        xlsdata.Longitude ./= 1e4
-        xlsdata.feet .*= 1000
-        CSV.write(splitext(joinpath(outfolder, folder, file))[1]*".dat", xlsdata, delim='\t')
-      catch
-        @warn "Error in reading data from $folder/$file. Possibly empty Excel sheets. Data skipped."
-      end
-    end
-  end
+  prepare_outfolder(outfolder)
+  files = String[]; folders = String[]
+  files, folders = findFiles(files, folders, "data/", infolder, ".xlsx")
+  processXLS(files, folders, infolder, outfolder, sheetname)
 end #function correctXLS
 
 end # module
